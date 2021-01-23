4ba70938b913c91eac29c70ab99a1c850d4c979e
jesusa2624 <jesusa2624@gmail.com>
2021-01-22 22:11:50 -0500

    noche 22/01/2021

--- database/migrations/2021_01_23_004000_create_visitas_table.php
+++ database/migrations/2021_01_23_004000_create_visitas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateVisitasPertenenciasTable extends Migration
+class CreateVisitasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateVisitasPertenenciasTable extends Migration
      */
     public function up()
     {
-        Schema::create('visitas_pertenencias', function (Blueprint $table) {
+        Schema::create('visitas', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateVisitasPertenenciasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('visitas_pertenencias');
+        Schema::dropIfExists('visitas');
     }
 }
